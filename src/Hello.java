2e0878caf9285df1dd70aa807dc5ea855d8e2745
Nikita Awasthi <nikita.awasthi@blueoptima.com>
2021-03-03 05:03:09 +0000

    commit

--- src/Hello.java
+++ src/Hello.java
@@ -13,7 +13,7 @@ public class Hello {
     System.out.println("simple merge + deletion later");
     System.out.println("squash merge + deletion together(sorry the last one was also squash merge, not simple merge) with TestBranch10");
     System.out.println("double check to make sure squash merge + delete later will not give deleteSourceBranch");
-
+//c1
 
 
 
